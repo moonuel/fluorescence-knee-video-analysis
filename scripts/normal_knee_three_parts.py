2cfabe33f61eb69beedb239900041aa27bbbb429
moonuel <co.manuel@hotmail.com>
2025-05-01 15:04:56 -0400

    Implemented coords loader for normal knee

--- scripts/normal_knee_three_parts.py
+++ scripts/normal_knee_three_parts.py
@@ -6,24 +6,58 @@ if module_path not in sys.path: # Add to sys.path if not already present
 import numpy as np
 import pandas as pd
 import cv2
-from tifffile import imread as tif_imread
 import utils
 from typing import Tuple, Dict, List
 import matplotlib.pyplot as plt
 import aging_knee_three_parts as aktp
 
+VERBOSE = True
+
 def load_avi(fn) -> np.ndarray:
-    
+    if VERBOSE: print("load_avi() called!")
 
+    cap = cv2.VideoCapture(fn)
+    video = []
+    while cap.isOpened():
 
-    return
+        ret, frame = cap.read() # Returns (boolean, bgr frame)
+        if not ret: break
+
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        video.append(frame)
+
+    cap.release()
+    video = np.array(video)
+    return video
+
+def load_normal_knee_coords(fn:str, sheet_num:int) -> pd.DataFrame:
+    if VERBOSE: print("load_normal_knee_coords() called!")
+
+    coords = pd.read_excel(fn, engine="openpyxl", usecols="B,D,E")
+
+    coords["Frame Number"].ffill(inplace=True)    
+    coords.set_index("Frame Number")
+    coords.drop("Frame Number", axis=1, inplace=True)
+
+    assert coords.isnull().values.any() == 0
+    return coords
 
 def main():
-    
+    if VERBOSE: print("main() called!")
+
     # Load video
     video = load_avi("../data/video_1.avi")
 
+    # Load coords
+    coords = load_normal_knee_coords("../data/xy coordinates for knee imaging 0913.xlsx", 3)
+    coords.info()
+
+    
 
 
 if __name__ == "__main__":
     main()
+
+# TODO: wrap coords data in a Coords object to enforce the existence of certain metadata? 
+# ^ could subclass the Coords object to have more descriptive names based on the sheet that is selected? idk 
