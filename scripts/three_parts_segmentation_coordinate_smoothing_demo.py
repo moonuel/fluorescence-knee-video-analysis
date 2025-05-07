43f8e720f736606fdb962b70c60119bbc7ea324a
moonuel <co.manuel@hotmail.com>
2025-05-07 15:20:52 -0400

    Finished demo file for validating smoothed coordinates procedure

--- scripts/three_parts_segmentation_coordinate_smoothing_demo.py
+++ scripts/three_parts_segmentation_coordinate_smoothing_demo.py
@@ -6,11 +6,18 @@ from src.config import VERBOSE
 def main():
     if VERBOSE: print("main() called!")
 
-    # Load example video
+    # Load example video and coords
     video = io.load_nparray("../data/processed/aging_knee_processed.npy")
+    translation_mxs = io.load_nparray("../data/processed/aging_translation_mxs.npy")
+    coords, metadata = io.load_aging_knee_coords("../data/198_218 updated xy coordinates for knee-aging 250426.xlsx", 1)
+    coords = ks.translate_coords(translation_mxs, coords) # Center the coords
 
-    # Show video
-    views.show_frames(video)
+    # Smooth coords
+    coords_smthd = ks.smooth_coords(coords, 5)
+
+    # Validate smoothing procedure
+    views.plot_coords(video, coords, title="unsmoothed coordinates")
+    views.plot_coords(video, coords_smthd, title="smoothed coordinates")
 
 if __name__ == "__main__":
     main()
