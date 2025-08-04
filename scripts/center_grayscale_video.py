5eb50e0668a2783b6db8cc2d1e1547a1f15d555c
moonuel <co.manuel@hotmail.com>
2025-08-04 15:20:02 -0400

    New file for centering generic video data

---
+++ scripts/center_grayscale_video.py
@@ -0,0 +1,33 @@
+"""
+Loads, centers, and saves a raw grayscale knee video.
+"""
+
+import core.radial_segmentation as rdl
+import core.knee_segmentation as ks
+import utils.utils as utils
+import utils.io as io
+import utils.views as views
+import numpy as np
+from typing import Tuple
+
+
+
+
+
+def main(file_path:str, frames:Tuple[int, int], save_path:str):
+
+    video = io.load_hdf5_video_chunk(file_path, frames, verbose=True)
+
+    video, _ = rdl.centre_video_mp(video)
+
+    views.show_frames(views.rescale_video(video, scale_factor=0.5, show_video=False, show_num=False))
+
+    # io.sa
+
+    return
+
+if __name__ == "__main__":
+    file_path = "../data/raw/1 con-0 min-fluid movement_00001190.h5"
+    frames = None # None defaults to all
+
+    main(file_path, frames, "")
