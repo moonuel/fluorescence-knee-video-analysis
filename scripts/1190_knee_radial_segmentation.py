384b00d689d283325e298fe8d90ec88c43a4413d
moonuel <co.manuel@hotmail.com>
2025-08-06 15:55:55 -0400

    Got good interior boundary

--- scripts/1190_knee_radial_segmentation.py
+++ scripts/1190_knee_radial_segmentation.py
@@ -2,9 +2,62 @@ from utils import utils, io, views
 from core import knee_segmentation as ks
 from core import radial_segmentation as rdl
 import numpy as np
+from functools import partial
+import cv2
 
+"""
+Steps:
+    >>> Get femur mask
+    >>> Estimate femur tip
+    >>> Estimate femur midpoint
+    >>> Get radial segmentation
+"""
 
+def get_femur_mask(video:np.ndarray) -> np.ndarray:
 
+    video_blr = utils.parallel_process_video(video, utils.blur_video, num_workers=4, batch_size=150)
+    # views.show_frames(video_blr)
+
+    # Rescale intensities for more consistent otsu segmentation
+    ref_fm = video[492]
+    match_histograms_func = partial(rdl.match_histograms_video, reference_frame=ref_fm) # Establish pure function
+    video_blr_hist = utils.parallel_process_video(video_blr, match_histograms_func, verbose=True, num_workers=4, batch_size=100) # Parallelize 
+    # views.show_frames(video_blr_hist)
+
+    # Get outer mask 
+    otsu_func = partial(ks.get_otsu_masks, thresh_scale=0.6)
+    outer_mask = utils.parallel_process_video(video_blr_hist, otsu_func, verbose=True, num_workers=4, batch_size=100)
+    
+    morph_erode_func = partial(utils.morph_erode, kernel_size=(41,41))
+    outer_mask = utils.parallel_process_video(outer_mask, morph_erode_func) #, batch_size=100, num_workers=4) # Parallel version
+    # outer_mask = utils.morph_erode(outer_mask, (36,36)) # Non-parallel version
+
+    # views.draw_mask_boundary(video, outer_mask)
+
+    # Get inner mask
+    inner_mask = ks.mask_adaptive(video_blr, 141, 6.5)
+
+    # Exclude noise from inner mask
+    femur_mask = rdl.interior_mask(outer_mask, inner_mask)
+
+    # Refinements
+    femur_mask = utils.morph_open(femur_mask, (11,11))
+
+    femur_mask = utils.blur_video(femur_mask, (15,15)) # Smooth/expand edges
+    femur_mask = (femur_mask > 0).astype(np.uint8) * 255 
+    
+    femur_mask = utils.morph_close(femur_mask, (27,23))
+
+    # Manual refinements 
+    femur_mask[:, 341: , :] = 0 # Cut y below 341
+    femur_mask[:, 0:132, :] = 0 # Cut y above 132
+    femur_mask[:, :, 291:] = 0 # Cut x above 291 
+    femur_mask[445:475, :, 278: ] = 0 # Between 445 and 474, cut x above 278
+    femur_mask[521:540, :, 282: ] = 0 # Between 521 and 539, cut x above 282
+
+    # views.show_frames(femur_mask)
+
+    return femur_mask
 
 def main():
 
@@ -13,10 +66,20 @@ def main():
     video = np.rot90(video, k=1, axes=(1,2))
     video = np.flip(video, axis=2)
 
-    views.show_frames(video)
+    # Fill empty regions with L=18
+    video[video == 0] = 18
 
-    # v1 = views.rescale_video(video, 0.5, False)
-    # views.show_frames(v1)
+    # views.show_frames(video)
+
+    # Get good femur mask
+    mask = get_femur_mask(video)
+
+    # Get interior boundary points
+    femur_boundary = rdl.sample_femur_interior_pts(mask, 128)
+
+    views.draw_points(video, femur_boundary)
+
+
 
 
 if __name__ == "__main__":
