52509b5b595e699e251c7c504a6c09eed05fd071
moonuel <co.manuel@hotmail.com>
2025-08-19 17:07:24 -0400

    Used on 1193 file

--- scripts/center_grayscale_video.py
+++ scripts/center_grayscale_video.py
@@ -33,8 +33,8 @@ def main(file_path:str, frames:Tuple[int, int], save_path:str):
         # "1190_knee_frames_ctrd.npy")
 
 if __name__ == "__main__":
-    h5_path = "../data/raw/1 con-0 min-fluid movement over exposure_00001189.h5"
-    npy_path = "../data/processed/1189_knee_frames_ctrd.npy"
+    h5_path = "../data/raw/1 con-10 min slow and quick joint movement tiny movement with quick_00001193.h5"
+    npy_path = "../data/processed/1193_knee_frames_ctrd.npy"
 
     frames = None # None defaults to all
 
